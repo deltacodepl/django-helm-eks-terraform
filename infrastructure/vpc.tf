e08bfc9a52fb4cc94fb454ff76e9670bec0a8097
Krzysztof Olszewski <hello@deltacode.pl>
2023-01-12 20:19:06 +0100

    vpc bump 3.18

--- infrastructure/vpc.tf
+++ infrastructure/vpc.tf
@@ -3,16 +3,20 @@ data "aws_availability_zones" "available" {
 }
 
 module "vpc" {
+  # https://registry.terraform.io/modules/terraform-aws-modules/vpc/aws/latest
   source  = "terraform-aws-modules/vpc/aws"
-  version = "3.2.0"
+  version = "3.18.1"
 
   name                 = "${var.app_name}-vpc"
   cidr                 = "10.0.0.0/16"
   azs                  = local.azs
   private_subnets      = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 10)]
   public_subnets       = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k)]
+  
   enable_nat_gateway   = true
   single_nat_gateway   = true
+  one_nat_gateway_per_az = false
+  enable_dns_support   = true
   enable_dns_hostnames = true
   
   
